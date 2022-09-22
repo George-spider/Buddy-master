bb36911307a9331253f5c226b7e560030814f988
weiyc <2950483363@qq.com>
2022-09-22 09:07:56 +0800

    Initial commit

---
+++ Buddy/src/Test.java
@@ -0,0 +1,13 @@
+import static java.lang.Math.pow;
+
+public class Test {
+    public static void main(String[] args) {
+        int a ;
+        a = (int) pow(2, (int) Math.ceil(Math.log(3) / Math.log(2)));
+
+        System.out.println(Math.log(1));
+        System.out.println(Math.log(3) / Math.log(2));
+        System.out.println(Math.ceil(1.5849625007211563));
+        System.out.println(a);
+    }
+}
